99d700a2c776c40b9610ca3bd87eb73a3527cf08
mhdip <ablaze.dip@gmail.com>
2019-02-02 18:08:52 +0600

    factory without like is complete

--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -1,9 +1,11 @@
 <?php
 
 use Faker\Generator as Faker;
-
+use App\User;
 $factory->define(App\Model\Like::class, function (Faker $faker) {
     return [
-        //
+        'User_id' => function(){
+            return User::all()->random();
+        },
     ];
 });
--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -1,9 +1,16 @@
 <?php
 
 use Faker\Generator as Faker;
-
+use App\Model\Question;
+use App\User;
 $factory->define(App\Model\Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text,
+        'question_id' => function(){
+            return Question::all()->random();
+        },
+        'User_id' => function(){
+            return User::all()->random();
+        },
     ];
 });
