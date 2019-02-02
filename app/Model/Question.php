5aa5f0203a2d4dbdcf79bade6bda725b903d41a6
mhdip <ablaze.dip@gmail.com>
2019-02-02 17:47:34 +0600

    model relationship done

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id');
+    }
+
+    public function replies()
+    {
+        return $this->hasMany('Reply', 'question_id', 'id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo('Category', 'question_id', 'id');
+    }
 }
--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function question()
+    {
+        return $this->belongsTo('Question', 'question_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id');
+    }
+
+    public function likes()
+    {
+        return $this->hasMany('Like');
+    }
 }
