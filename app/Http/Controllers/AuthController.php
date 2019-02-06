e1cbd8666c1e68c267e5c7743d0a98263fd62b4b
mhdip <ablaze.dip@gmail.com>
2019-02-06 17:48:22 +0600

    jwt signup done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use App\User;
 
 class AuthController extends Controller
 {
@@ -15,7 +16,7 @@ class AuthController extends Controller
     public function __construct()
     {
         // $this->middleware('auth:api', ['except' => ['login']]);
-        $this->middleware('jwt', ['except' => ['login']]);
+        $this->middleware('jwt', ['except' => ['login', 'signup']]);
     }
 
     /**
@@ -32,6 +33,12 @@ class AuthController extends Controller
         }
 
         return $this->respondWithToken($token);
+    }
+
+    public function signup(Request $request)
+    {
+        User::create($request->all());
+        return $this->login($request);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::group([
 
 ], function ($router) {
 
+    Route::post('signup', 'AuthController@signup');
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
