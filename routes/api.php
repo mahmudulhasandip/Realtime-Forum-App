eb31b85dfb89c15a29813237b4dd6dfa697727a2
mhdip <ablaze.dip@gmail.com>
2019-02-06 15:15:04 +0600

    Like and dislike for reply api create done

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,6 @@ use Illuminate\Http\Request;
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 Route::apiResource('/question/{question}/reply', 'ReplyController');
+
+Route::post('/like/{reply}', 'LikeController@likeIt');
+Route::delete('/like/{reply}', 'LikeController@unLikeIt');
