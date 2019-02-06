01dc9afa4a813c18ef562390b2b5f846f0a34e52
mhdip <ablaze.dip@gmail.com>
2019-02-06 17:23:54 +0600

    jwt installed and configured with exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,9 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +49,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof TokenInvalidException) {
+            return response()->json(['error' => 'Token is Invalid'], 400);
+        } elseif ($exception instanceof TokenExpiredException) {
+            return response()->json(['error' => 'Token is Expired'], 400);
+        } elseif ($exception instanceof JWTException) {
+            return response()->json(['error' => 'There is problem with your token'], 400);
+        }
+
         return parent::render($request, $exception);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,20 @@ use Illuminate\Http\Request;
 // });
 
 
+Route::group([
+
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('payload', 'AuthController@payload');
+
+});
+
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 Route::apiResource('/question/{question}/reply', 'ReplyController');
