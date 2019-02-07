4b6d14b2acd5945f6a04dcb371264f7c29073353
mhdip <ablaze.dip@gmail.com>
2019-02-07 11:02:43 +0600

    Installed Vuetify, vue-router, and initial components setup

---
+++ resources/js/Router/router.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+
+const routes = [{
+    // path: '/login',
+    // component: Login
+}]
+
+const router = new VueRouter({
+    routes
+})
+
+export default router;
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,17 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+import Vue from "vue";
+import Vuetify from "vuetify";
+import router from './Router/router';
+
+window.Vue = require("vue");
+Vue.use(Vuetify);
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,14 +24,10 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component("AppHome", require("./components/AppHome.vue").default);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
 const app = new Vue({
-    el: '#app'
+    el: "#app",
+    router
 });
